dade1f4e47ff43bdffe763ddf9e81d4416fec293
eunjismail <eunjismail@gmail.com>
2025-08-06 11:17:35 +0900

    dddd

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      supportedLocales: [
-        Locale('ko', 'KR')
-      ],
+      supportedLocales: [Locale('ko', 'KR')],
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
@@ -37,7 +35,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -48,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   int _currentPageIndex = 0;
   late TabController _tabController;
 
-  @override 
+  @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 5, vsync: this);
@@ -60,40 +57,47 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     super.dispose();
   }
 
-
   Color? _thumbColor = Color.fromRGBO(217, 217, 217, 1);
   double _currentDiscreteSliderValue = 0;
 
-  Color? getThumbColor (double value) {
+  Color? getThumbColor(double value) {
     if (value == -10) {
       return Color.fromRGBO(255, 95, 98, 1);
-    }
-    else if (value < -5) {
+    } else if (value < -5) {
       double t = (value + 10) / 5;
-      return Color.lerp(Color.fromRGBO(255, 95, 98, 1), Color.fromRGBO(255, 165, 100, 1), t);
-    }
-    else if (value == -5) {
+      return Color.lerp(
+        Color.fromRGBO(255, 95, 98, 1),
+        Color.fromRGBO(255, 165, 100, 1),
+        t,
+      );
+    } else if (value == -5) {
       return Color.fromRGBO(255, 165, 100, 1);
-    }
-    else if (value < 0) {
+    } else if (value < 0) {
       double t = (value + 5) / 5;
-      return Color.lerp(Color.fromRGBO(255, 165, 100, 1), Color.fromRGBO(217, 217, 217, 1), t);
-    }
-    else if (value == 0) {
+      return Color.lerp(
+        Color.fromRGBO(255, 165, 100, 1),
+        Color.fromRGBO(217, 217, 217, 1),
+        t,
+      );
+    } else if (value == 0) {
       return Color.fromRGBO(217, 217, 217, 1);
-    }
-    else if (value < 5) {
+    } else if (value < 5) {
       double t = (value) / 5;
-      return Color.lerp(Color.fromRGBO(217, 217, 217, 1), Color.fromRGBO(136, 238, 209, 1), t);
-    }
-    else if (value == 5) {
+      return Color.lerp(
+        Color.fromRGBO(217, 217, 217, 1),
+        Color.fromRGBO(136, 238, 209, 1),
+        t,
+      );
+    } else if (value == 5) {
       return Color.fromRGBO(136, 238, 209, 1);
-    }
-    else if (value < 10) {
-      double t = (value-5) / 5;
-      return Color.lerp(Color.fromRGBO(136, 238, 209, 1), Color.fromRGBO(148, 198, 255, 1), t);
-    }
-    else if (value == 10) {
+    } else if (value < 10) {
+      double t = (value - 5) / 5;
+      return Color.lerp(
+        Color.fromRGBO(136, 238, 209, 1),
+        Color.fromRGBO(148, 198, 255, 1),
+        t,
+      );
+    } else if (value == 10) {
       return Color.fromRGBO(148, 198, 255, 1);
     }
     return _thumbColor;
@@ -111,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           Center(),
           NotificationPage(),
           Center(),
-        ]
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Color(0xffFCFAF5),
@@ -124,25 +128,24 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             icon: Icon(
               Icons.home,
               color: _currentPageIndex == 0 ? Colors.black : Colors.grey,
-            ), 
-            label: '홈'
+            ),
+            label: '홈',
           ),
           BottomNavigationBarItem(
             icon: Icon(
               Icons.analytics,
               color: _currentPageIndex == 1 ? Colors.black : Colors.grey,
-            ), 
-            label: '분석'
+            ),
+            label: '분석',
           ),
           BottomNavigationBarItem(
             icon: Icon(
               Icons.feed,
               color: _currentPageIndex == 2 ? Colors.black : Colors.grey,
-            ), 
-            label: '피드'
+            ),
+            label: '피드',
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: '알림'
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: '알림'),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: '마이페이지'),
         ],
         currentIndex: _currentPageIndex,
@@ -157,14 +160,11 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   }
 }
 
-TextStyle _textStyle = TextStyle(
-  fontFamily: 'gangwon',
-  fontSize: 15,
-);
+TextStyle _textStyle = TextStyle(fontFamily: 'gangwon', fontSize: 15);
 
 TextStyle _emotion = TextStyle(
   fontFamily: 'gangwon',
   fontWeight: FontWeight.bold,
   fontSize: 11,
-  letterSpacing: 0
+  letterSpacing: 0,
 );
