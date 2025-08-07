2b46ae32222c97c11308f901030f9c9e69368198
eunjismail <eunjismail@gmail.com>
2025-08-07 15:42:21 +0900

    E

--- lib/main3.dart
+++ lib/main3.dart
@@ -15,114 +15,115 @@ class ProfileMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: [
-        ProfileMy(
-          main: 'assets/profile1.png',
-          name: '고먐미',
-          friend: '11',
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Edit()),
-            );
-          },
-        ),
-        Divider(color: Color(0xffD4D4D4)),
-        SizedBox(height: 7),
-        Padding(
-          padding: EdgeInsets.only(left: 22),
-          child: Text(
-            '내가 쓴 일기',
-            style: TextStyle(fontSize: 16, fontFamily: 'gangwon'),
+        children: [
+          ProfileMy(
+            main: 'assets/profile1.png',
+            name: '고먐미',
+            friend: '11',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Edit()),
+              );
+            },
           ),
-        ),
-        ProfileListLast(
-          happliy: 'assets/happily/4.png',
-          photo: 'assets/list/1.png',
-          title: diaryTitle1,
-          text: diaryTitle2,
-          date: '2025.7.30',
-          public: '공개',
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) =>
-                    Write(diaryTitle1: diaryTitle1, diaryTitle2: diaryTitle2),
-              ),
-            );
-          },
-        ),
-        // ProfileList(
-        //   happliy: 'assets/happily/4.png',
-        //   photo: 'assets/list/1.png',
-        //   title: '이별한지 23일',
-        //   text: '너가 없으니 내 행복도 사라졌다',
-        //   text2: '잘지내길...ㅜ',
-        //   date: '2025.7.30',
-        //   public: '공개',
-        //   onTap: () {
-        //     Navigator.push(
-        //       context,
-        //       MaterialPageRoute(builder: (context) => Write()),
-        //     );
-        //   },
-        // ),
-        ProfileList(
-          happliy: 'assets/happily/4.png',
-          photo: 'assets/list/2.png',
-          title: '기말고사 후기',
-          text: '재수강 각 떴다',
-          text2: '하.......',
-          date: '2025.7.18',
-          public: '공개',
-        ),
-        ProfileList(
-          happliy: 'assets/happily/4.png',
-          photo: 'assets/list/3.png',
-          title: '선풍기 당근',
-          text: '선풍기를 샀다!!',
-          text2: '원래 27,900원인데 9,000원에 get~~~...',
-          date: '2025.7.13',
-          public: '공개',
-        ),
-        ProfileList(
-          happliy: 'assets/happily/4.png',
-          photo: 'assets/list/4.png',
-          title: '오늘 먹은 거',
-          text: '오늘 점심에 명성 떡볶이를 먹었따',
-          text2: '완전 맛있었다. 행복한 하루~~~...',
-          date: '2025.7.9',
-          public: '공개',
-        ),
-        ProfileList(
-          happliy: 'assets/happily/4.png',
-          photo: 'assets/list/5.png',
-          title: '감사 / 행복',
-          text: '오늘 감사했던 일 3가지는?',
-          text2: '일어남에 감사, 걸음에 감사, 숨쉴 수...',
-          date: '2025.7.4',
-          public: '공개',
-        ),
-        ProfileList(
-          happliy: 'assets/happily/4.png',
-          photo: 'assets/list/6.png',
-          title: '등산은 배움의 장터!',
-          text: '여자친구와 등산 데이트다잉...~^^',
-          text2: '맑은 공기.. 맑은 구름.. 힐링 스팟이다잉',
-          date: '2025.7.1',
-          public: '공개',
-        ),
-        ProfileList(
-          happliy: 'assets/happily/4.png',
-          photo: 'assets/list/7.png',
-          title: '사건 / 생각 / 감정',
-          text: '집사러 갔다가 소다 학회 늦었음;;ㅎ 완전',
-          text2: '완전 망했다고 생각..;; 진땀이 뻘뻘나더...',
-          date: '2025.6.22',
-          public: '공개',
-        ),
-      ],
+          Divider(color: Color(0xffD4D4D4)),
+          SizedBox(height: 7),
+          Padding(
+            padding: EdgeInsets.only(left: 22),
+            child: Text(
+              '내가 쓴 일기',
+              style: TextStyle(fontSize: 16, fontFamily: 'gangwon'),
+            ),
+          ),
+          ProfileListLast(
+            happliy: 'assets/happily/4.png',
+            photo: 'assets/list/1.png',
+            title: diaryTitle1,
+            text: diaryTitle2,
+            date: '2025.7.30',
+            public: '공개',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      Write(diaryTitle1: diaryTitle1, diaryTitle2: diaryTitle2),
+                ),
+              );
+            },
+          ),
+          // ProfileList(
+          //   happliy: 'assets/happily/4.png',
+          //   photo: 'assets/list/1.png',
+          //   title: '이별한지 23일',
+          //   text: '너가 없으니 내 행복도 사라졌다',
+          //   text2: '잘지내길...ㅜ',
+          //   date: '2025.7.30',
+          //   public: '공개',
+          //   onTap: () {
+          //     Navigator.push(
+          //       context,
+          //       MaterialPageRoute(builder: (context) => Write()),
+          //     );
+          //   },
+          // ),
+          ProfileList(
+            happliy: 'assets/happily/4.png',
+            photo: 'assets/list/2.png',
+            title: '기말고사 후기',
+            text: '재수강 각 떴다',
+            text2: '하.......',
+            date: '2025.7.18',
+            public: '공개',
+          ),
+          ProfileList(
+            happliy: 'assets/happily/4.png',
+            photo: 'assets/list/3.png',
+            title: '선풍기 당근',
+            text: '선풍기를 샀다!!',
+            text2: '원래 27,900원인데 9,000원에 get~~~...',
+            date: '2025.7.13',
+            public: '공개',
+          ),
+          ProfileList(
+            happliy: 'assets/happily/4.png',
+            photo: 'assets/list/4.png',
+            title: '오늘 먹은 거',
+            text: '오늘 점심에 명성 떡볶이를 먹었따',
+            text2: '완전 맛있었다. 행복한 하루~~~...',
+            date: '2025.7.9',
+            public: '공개',
+          ),
+          ProfileList(
+            happliy: 'assets/happily/4.png',
+            photo: 'assets/list/5.png',
+            title: '감사 / 행복',
+            text: '오늘 감사했던 일 3가지는?',
+            text2: '일어남에 감사, 걸음에 감사, 숨쉴 수...',
+            date: '2025.7.4',
+            public: '공개',
+          ),
+          ProfileList(
+            happliy: 'assets/happily/4.png',
+            photo: 'assets/list/6.png',
+            title: '등산은 배움의 장터!',
+            text: '여자친구와 등산 데이트다잉...~^^',
+            text2: '맑은 공기.. 맑은 구름.. 힐링 스팟이다잉',
+            date: '2025.7.1',
+            public: '공개',
+          ),
+          ProfileList(
+            happliy: 'assets/happily/4.png',
+            photo: 'assets/list/7.png',
+            title: '사건 / 생각 / 감정',
+            text: '집사러 갔다가 소다 학회 늦었음;;ㅎ 완전',
+            text2: '완전 망했다고 생각..;; 진땀이 뻘뻘나더...',
+            date: '2025.6.22',
+            public: '공개',
+          ),
+        ],
+
     );
   }
 }
